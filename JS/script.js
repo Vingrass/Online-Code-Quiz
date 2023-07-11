2ce197d4e0d5a161c2674406196decd92eebc1d6
Vingrass <ingrassiav99@gmail.com>
2023-07-10 19:03:17 -0700

    added start button. edited css/js so buttons change with question

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,9 @@
+var startBtn = document.querySelector("#start");
 var ans1Btn = document.querySelector("#Ans1");
 var ans2Btn = document.querySelector("#Ans2");
 var ans3Btn = document.querySelector("#Ans3");
 var ans4Btn = document.querySelector("#Ans4");
+
 
 var questionNum = 0;
 var questions = ["What does CSS stand for?","What does HTML stand for?"] ;
@@ -11,12 +13,27 @@ var answerSetTwo =["Cool Sister Sam", "Beans"]
 var answerSetThree =["California Sea Side", "Beans"]
 var answerSetFour =["Colossal Submarine Sandwhich", "Beans"]
 
+start.addEventListener("click", startQuiz);
 ans1Btn.addEventListener("click", nextQuestion);
 ans2Btn.addEventListener("click", nextQuestion);
 ans3Btn.addEventListener("click", nextQuestion);
 ans4Btn.addEventListener("click", nextQuestion);
 
+
+function startQuiz(){
+    document.getElementById("start").style.display = "none";
+    nextQuestion();
+}
+
+
+
 function nextQuestion(){
+
+    document.getElementById("Ans1").style.display = "inline-block";
+    document.getElementById("Ans2").style.display = "inline-block";
+    document.getElementById("Ans3").style.display = "inline-block";
+    document.getElementById("Ans4").style.display = "inline-block";
+
     document.getElementById("Question").innerHTML = questions[questionNum];
     document.getElementById("Ans1").innerHTML = answerSetOne[questionNum];
     document.getElementById("Ans2").innerHTML = answerSetTwo[questionNum];
