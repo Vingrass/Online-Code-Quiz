666ba5cee4b1049e3c4a648965ce9b9c70baf894
Vingrass <ingrassiav99@gmail.com>
2023-07-10 17:54:33 -0700

    Added more buttons. Added Question & Answer list.

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,29 @@
-generateBtn.addEventListener("click", nextQuestion);
+var ans1Btn = document.querySelector("#Ans1");
+var ans2Btn = document.querySelector("#Ans2");
+var ans3Btn = document.querySelector("#Ans3");
+var ans4Btn = document.querySelector("#Ans4");
+
+var questionNum = 0;
+var questions = ["What does CSS stand for?","What does HTML stand for?"] ;
+
+var answerSetOne =["Cascading Style Sheet", "Beans"]
+var answerSetTwo =["Cool Sister Sam", "Beans"]
+var answerSetThree =["California Sea Side", "Beans"]
+var answerSetFour =["Colossal Submarine Sandwhich", "Beans"]
+
+ans1Btn.addEventListener("click", nextQuestion);
+ans2Btn.addEventListener("click", nextQuestion);
+ans3Btn.addEventListener("click", nextQuestion);
+ans4Btn.addEventListener("click", nextQuestion);
 
 function nextQuestion(){
-    document.getElementById("Question").innerHTML = "What does CSS stand for?";
+    document.getElementById("Question").innerHTML = questions[questionNum];
+    document.getElementById("Ans1").innerHTML = answerSetOne[questionNum];
+    document.getElementById("Ans2").innerHTML = answerSetTwo[questionNum];
+    document.getElementById("Ans3").innerHTML = answerSetThree[questionNum];
+    document.getElementById("Ans4").innerHTML = answerSetFour[questionNum];
+
+    questionNum++
 
     return
 }
