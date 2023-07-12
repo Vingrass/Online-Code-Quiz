fa95b180693e84765d8b97511bb4d922e22a483f
Vingrass <ingrassiav99@gmail.com>
2023-07-11 19:14:03 -0700

    Fixed timer to adjucts to in/correct answers. Added more questions/answers

--- JS/script.js
+++ JS/script.js
@@ -6,14 +6,24 @@ var ans4Btn = document.querySelector("#Ans4");
 
 
 var questionNum = 0;
-var questions = ["What does CSS stand for?","What does HTML stand for?"] ;
+var questions = ["What does CSS stand for?","What does HTML stand for?", "Which is NOT a data type?", "Array data is contained withing:",
+                 "How do you reference a class in CSS?",    "How do you reference an ID in CSS?",      "What does API stand for?" ] ;
 
-var ansSetOne =["Cascading Style Sheet", "Beans"]
-var ansSetTwo =["Cool Sister Sam", "Beans"]
-var ansSetThree =["California Sea Side", "Beans"]
-var ansSetFour =["Colossal Submarine Sandwhich", "Beans"]
+var ansSetOne =["Cascading Style Sheet",        "Houston Texas Movie Lovers",   "String",   "Brackets []",
+                "-ClassName",                   "-IDname",                      "Application Pass Interference"];
+var ansSetTwo =["Cool Sister Sam",              "Hypertext Markup Language",    "Number",   "Parenthesis ()",
+                "#ClassName",                   "#IDname",                      "Access Programming Imitation"];
+var ansSetThree =["California Sea Side",        "Hot To My Lips",               "Booling",  "Curly Brackets {}",
+                  ">ClassName",                 ">IDname",                      "Application Programming Interface"];
+var ansSetFour =["Colossal Submarine Sandwhich", "Hyper Text Moving Language",  "Array",    "Two slices of bread",
+                 ".ClassName",                  ".IDname",                      "Apocalyptic PetroDragonic Infinity"];
 
-var ansKey = [0,1,2,3];
+var ansKey = [0,1,2,3,1,4,2,3];
+var usrScore = 0;
+
+var countDownTime = 0;
+var timeLeft;
+var now;
 
 start.addEventListener("click", startQuiz);
 ans1Btn.addEventListener("click", () => nextQuestion(1));
@@ -24,25 +34,23 @@ ans4Btn.addEventListener("click", () => nextQuestion(4));
 //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 function startQuiz(){
     document.getElementById("start").style.display = "none";
-    document.getElementById("Ans1").style.display = "inline-block";
-    document.getElementById("Ans2").style.display = "inline-block";
-    document.getElementById("Ans3").style.display = "inline-block";
-    document.getElementById("Ans4").style.display = "inline-block";
+    document.getElementById("Ans1").style.display = "block";
+    document.getElementById("Ans2").style.display = "block";
+    document.getElementById("Ans3").style.display = "block";
+    document.getElementById("Ans4").style.display = "block";
 
-    countDownClock(0);
+    countDownTime = new Date().getTime() + 66000;
+    countDownClock();
     nextQuestion(0);
 }
 
 //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-function countDownClock(timeAdd){
-
-    var countDownTime = new Date().getTime() + 66000;
-
+function countDownClock(){
+    
     setInterval(function(){
         
         var now = new Date().getTime();
         var timeLeft = countDownTime - now;
-            timeLeft += timeAdd;
 
         document.getElementById("TimerID").innerHTML = "Time left: " + Math.floor(timeLeft/1000) + "s";
 
@@ -71,11 +79,10 @@ function nextQuestion(usrResp){
 //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 function checkAnswer(usrResp){
     if(usrResp == ansKey[questionNum]){
-        console.log("correct");
-        countDownClock(10000);
+        countDownTime += 10000
+        usrScore++
     }else{
-        console.log("WRONG");
-        countDownClock(-10000);
+        countDownTime -= 10000
     }
 }
 
